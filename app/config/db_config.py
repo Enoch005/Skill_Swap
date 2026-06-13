862114687e0aef4b7c3eb8aec91c2939d43dec58
Enoch <enoch5wadagle@gmail.com>
2026-06-13 16:03:15 +0530

    Day 2 completed - Flask, MySQL and homepage setup

---
+++ app/config/db_config.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="ENOCH@2005",
+        database="skillswap"
+    )
+
+    return connection
